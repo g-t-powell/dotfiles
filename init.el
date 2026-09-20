c1881198e3d11c3afad0e379065d3081fb45893f
Gabriel Powell <mail@gpowell.net>
2026-09-19 22:18:18 -0400

    update emacs init.el

--- init.el
+++ init.el
@@ -1,49 +1,76 @@
-;; org mode settings
+;; packages
+(require 'package)
+(setq package-archives
+	'(("gnu" 		. "https://elpa.gnu.org/packages/")
+	  ("melpa"		. "https://melpa.org/packages/")))
+	  ; ("melpa-stable"	. "https://stable.melpa.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+	(package-refresh-contents))
+(unless (package-installed-p 'use-package)
+	(package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; appearance
+(global-visual-line-mode t)
+(setq frame-resize-pixelwise t)
+(tab-bar-mode 1)
+(tool-bar-mode -1)
+
+
+;; org mode
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
-
-;; agenda view
-(setq org-agenda-files '("~/org"))
-
-;; org agenda todo stati
+(setq org-agenda-files '("/mnt/remote/org"))
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELLED")))
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(package-initialize)
+;; evil
+(use-package evil
+	:config
+	(evil-mode 1))
+
+; tree-sitter
+(require 'tree-sitter)
+(require 'tree-sitter-langs)
+(setq treesit-language-source-alist
+      '((typst "https://github.com/uben0/tree-sitter-typst")))
+
+;; typst
+(use-package typst-ts-mode
+  :vc (:url "https://codeberg.org/meow_king/typst-ts-mode")
+  :custom
+  (typst-ts-mode-watch-options "--open"))
+(use-package websocket)
+(use-package typst-preview)
+
+
+
+;; language support
+(use-package markdown-mode)
+
+(use-package lsp-mode
+	:hook ((typst-ts-mode . lsp-deferred)
+	       (python-mode   . lsp-deferred)
+	       (cobol-mode    . lsp-deferred))
+		
+	:commands (lsp lsp-deferred)
+	:custom
+	(lsp-typst-export-pdf "onSave")
+	(lsp-typst-formatter-mode "typstyle"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil markdown-mode typst-ts-mode)))
+ '(package-selected-packages
+   '(evil lsp-mode tree-sitter-langs typst-preview typst-ts-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-
-(add-hook 'typst-ts-mode-hook #'eglot-ensure)
-
-(with-eval-after-load 'eglot
-  (add-to-list
-   'eglot-server-programs
-   '(typst-ts-mode . ("tinymist"))))
-
-(use-package websocket
-  :ensure t)
-
-(use-package typst-preview
-  :ensure t)
